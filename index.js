dcc0bf2dbbc99cd164cce1d79de96f3dec3dce47
Esfito <103140553+Esfito@users.noreply.github.com>
2025-12-14 20:14:11 -0300

    Corrijo error de auth global. Espero

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(express.json());
 app.use("/api", productsRouter);
 app.use("/api", authRouter);
 
-app.use(auth);
-
 app.use((req, res, next) => {
   res.status(404).json({ error: "Algo falló - Puede que no exista la ruta solicitada" });
 });
