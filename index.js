c115b19a418a331c4eb984a980d6e58c000188f4
Esfito <103140553+Esfito@users.noreply.github.com>
2025-12-14 18:55:06 -0300

    ajustado para vercel

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use((req, res, next) => {
   res.status(404).json({ error: "Algo falló - Puede que no exista la ruta solicitada" });
 });
 
-const PORT = process.env.PORT || 3005;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3005;
+  app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+}
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+export default app;
